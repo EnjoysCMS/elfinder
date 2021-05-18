8c1602afb5f0a8a55208d94da4e6b1cc490f2af1
enjoys <deadly.info@gmail.com>
2021-05-18 22:22:45 +0300

    check ENV

--- src/Controller.php
+++ src/Controller.php
@@ -7,13 +7,40 @@ namespace EnjoysCMS\Module\ElFinder;
 
 
 use App\Module\Admin\BaseController;
+use Doctrine\ORM\EntityManager;
+use Enjoys\Forms\Renderer\RendererInterface;
+use Enjoys\Http\ServerRequestInterface;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Twig\Environment;
 
 final class Controller extends BaseController
 {
 
     private string $templatePath = __DIR__;
+
+    /**
+     * @throws \Exception
+     */
+    public function __construct(
+        Environment $twig,
+        ServerRequestInterface $serverRequest,
+        EntityManager $entityManager,
+        UrlGeneratorInterface $urlGenerator,
+        RendererInterface $renderer
+    ) {
+        if (!isset($_ENV['ELFINDER_VENDOR_DIR'])
+            || !isset($_ENV['ELFINDER_FILES_DIR'])
+            || !isset($_ENV['ELFINDER_FILES_URL'])
+            || !isset($_ENV['ELFINDER_TRASH_DIR'])
+            || !isset($_ENV['ELFINDER_TRASH_URL'])
+        ) {
+            throw new \Exception('Please read README.md and set Environment variables');
+        }
+
+        parent::__construct($twig, $serverRequest, $entityManager, $urlGenerator, $renderer);
+    }
 
     /**
      * @return string
@@ -40,8 +67,9 @@ final class Controller extends BaseController
         ]
 
     )]
-    public function elFinder(ContainerInterface $container): string
-    {
+    public function elFinder(
+        ContainerInterface $container
+    ): string {
         return $this->view(
             $this->getTemplatePath() . '/elfinder.twig',
             $this->getContext($container->get(Admin::class))
